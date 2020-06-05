0d5ebc66bca8f6361f1f1b13e6f7389d5f62025a
Nihal Nurbhasha <Nihal@Nihals-MacBook-Pro.local>
2020-06-05 09:42:25 -0500

    transactions from state

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Transaction } from "./Transaction";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
+  // can pull out anything from GlobalContext using useContext hooks
+  // const context = useContext(GlobalContext);
+  // destructuring
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
+        {/* <li className="minus">
           Cash <span>-$400</span>
           <button className="delete-btn">x</button>
-        </li>
+        </li> */}
+        {/* vs.  */}
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
