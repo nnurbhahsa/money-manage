9ad49e78c43a760613ef1f1d895bf312b614666e
Nihal Nurbhasha <Nihal@Nihals-MacBook-Pro.local>
2020-06-06 08:51:34 -0500

    Updates

---
+++ precache-manifest.0b962e6f07262e525ef5376082785947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8051cdd1ab087dc6333db426ca2e3e",
+    "url": "/money-manage/index.html"
+  },
+  {
+    "revision": "85c81c953c996f9b4753",
+    "url": "/money-manage/static/css/main.95842ff4.chunk.css"
+  },
+  {
+    "revision": "c3723a190d5f2e40e202",
+    "url": "/money-manage/static/js/2.cccb0884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-manage/static/js/2.cccb0884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c81c953c996f9b4753",
+    "url": "/money-manage/static/js/main.b0f79a37.chunk.js"
+  },
+  {
+    "revision": "384d2aa2e8111ba03440",
+    "url": "/money-manage/static/js/runtime-main.5cbaf82a.js"
+  }
+]);
